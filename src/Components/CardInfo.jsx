58c904ee9300ce8c22e1f5b966e93f91e2b4a051
Cesar Zuluaga <cesar.zuluaga@aliatic.com.co>
2023-09-06 08:38:40 -0500

    cesar.zuluaga 2023.09.06 queries finished basic

--- src/Components/CardInfo.jsx
+++ src/Components/CardInfo.jsx
@@ -8,16 +8,35 @@ const defaultData = {
     acept: false
 }
 
+const OBTAIN_CBL_API_URL = `https://www.medellin.gov.co/site_consulta_pot/BuscarFichaMat.hyg?buscar=`
+
 const CardInfo = () => {
     const [load, setLoad] = React.useState(false)
     const [errors, setErrors] = React.useState({})
     const [formData, setFormData] = React.useState(defaultData)
+    const [features, setFeatures] = React.useState([])
 
     const fetchPOT = React.useRef(null)
 
     //RegExp
     const validarMatricula = /^[0-9]{1,8}$/g
     const validarMetros = /^[0-9]{1,6}$/g
+
+    function addDotThousands(numero) {
+        // Convertir el número a una cadena de texto
+        let numeroString = numero.toString();
+      
+        // Separar la parte entera de la parte decimal (si existe)
+        let partes = numeroString.split('.');
+        let parteEntera = partes[0];
+        let parteDecimal = partes.length > 1 ? ',' + partes[1] : '';
+      
+        // Agregar los puntos de mil a la parte entera
+        parteEntera = parteEntera.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+      
+        // Devolver el número con los puntos de mil agregados
+        return parteEntera + parteDecimal;
+    }
 
     const inputChangeHandler = e => {
         setErrors({})
@@ -69,57 +88,103 @@ const CardInfo = () => {
 
     const postData = async (formData) => {
         try {
-            const obtainCBL = await ApiService.obtainCBL(formData.matricula)
-            console.log(obtainCBL)
+            const obtainCBL = await ApiService.obtainCBL(OBTAIN_CBL_API_URL, formData.matricula)
+            if (obtainCBL.code !== '200') {
+                setLoad(false)
+                setFormData(defaultData)
+                return
+            }
+            const obtainPOT48Data = await ApiService.obtainPOT48Data(obtainCBL.data.cbml)
+            if (obtainPOT48Data.code !== '200') {
+                setLoad(false)
+                setFormData(defaultData)
+                return
+            }
+            // console.log(obtainPOT48Data.data.features[0].geometry.rings)
+            const ring1 = obtainPOT48Data.data.features[0].geometry.rings[0]
+            // console.log(obtainPOT48Data.data)
+            const xmin = ring1[0][0]
+            const ymin = ring1[0][1]
+            const xmax = ring1[3][0]
+            const ymax = ring1[3][1]
+
+            const spartialReference = obtainPOT48Data.data.spatialReference
+
+            const obtainM2value = await ApiService.obtainValueM2(xmin, ymin, xmax, ymax, spartialReference)
+
+            // console.log(obtainM2value)
+            setFeatures(obtainM2value.data.features)
             setLoad(false)
         } catch (error) {
             setLoad(false)
             console.log(error)
+            setFormData(defaultData)
         }
     }
 
     return (
-        <div className="card w-50 mx-auto">
-            <div className="card-header">
-                Palmaria Consulta
+        <>
+            <div className="card w-50 mx-auto">
+                <div className="card-header">
+                    Palmaria Consulta
+                </div>
+                {
+                    load ? <>
+                        <Sipinner />
+                    </> :
+                        <>
+                            <div className="card-body">
+                                <h5 className="card-title">Ingresa el código de la matricula y la cantidad de m2 a consultar.</h5>
+
+                                <form ref={fetchPOT} onSubmit={(e) => handleSubmit(e)}>
+                                    <div className="mb-3">
+                                        <label htmlFor="NumMatricula" className="form-label">Número de matricula</label>
+                                        <input name={"matricula"} value={formData.matricula} type="number" className="form-control" id="NumMatricula" aria-describedby="numMatriculaHelp" onChange={(e) => inputChangeHandler(e)} />
+                                        {
+                                            errors.matricula ? <div id="numMatriculaHelp" className="form-text text-danger">{errors.matricula}</div> : <div id="numMatriculaHelp" className="form-text">Por favor escriba el número de matricula, sólo se permiten caracteres numéricos.</div>
+                                        }
+
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="Metros" className="form-label">Cantidad de metros cuadrados</label>
+                                        <input name={"metros"} value={formData.metros} type="number" className="form-control" id="Metros" aria-describedby="metrosHelp" onChange={(e) => inputChangeHandler(e)} />
+                                        {
+                                            errors.metros ? <div id="metrosHelp" className="form-text text-danger">{errors.metros}</div> : <div id="metrosHelp" className="form-text">Por favor indique la cantidad de metros cuadrados por los que desea consultar.</div>
+                                        }
+                                    </div>
+                                    <div className="mb-3 form-check">
+                                        <input name={"acept"} type="checkbox" className="form-check-input" id="exampleCheck1" onChange={(e) => inputChangeHandler(e)} />
+                                        <label className="form-check-label" htmlFor="exampleCheck1">Acepta los términos y condiciones</label>
+                                        {
+                                            errors.acept && <div id="metrosHelp" className="form-text text-danger">{errors.acept}</div>
+                                        }
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Consultar</button>
+                                </form>
+                            </div>
+                        </>
+                }
             </div>
-            {
-                load ? <>
-                    <Sipinner />
-                </> :
-                    <>
-                        <div className="card-body">
-                            <h5 className="card-title">Ingresa el código de la matricula y la cantidad de m2 a consultar.</h5>
 
-                            <form ref={fetchPOT} onSubmit={(e) => handleSubmit(e)}>
-                                <div className="mb-3">
-                                    <label htmlFor="NumMatricula" className="form-label">Número de matricula</label>
-                                    <input name={"matricula"} value={formData.matricula} type="number" className="form-control" id="NumMatricula" aria-describedby="numMatriculaHelp" onChange={(e) => inputChangeHandler(e)} />
-                                    {
-                                        errors.matricula ? <div id="numMatriculaHelp" className="form-text text-danger">{errors.matricula}</div> : <div id="numMatriculaHelp" className="form-text">Por favor escriba el número de matricula, sólo se permiten caracteres numéricos.</div>
-                                    }
-
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="Metros" className="form-label">Cantidad de metros cuadrados</label>
-                                    <input name={"metros"} value={formData.metros} type="number" className="form-control" id="Metros" aria-describedby="metrosHelp" onChange={(e) => inputChangeHandler(e)} />
-                                    {
-                                        errors.metros ? <div id="metrosHelp" className="form-text text-danger">{errors.metros}</div> : <div id="metrosHelp" className="form-text">Por favor indique la cantidad de metros cuadrados por los que desea consultar.</div>
-                                    }
-                                </div>
-                                <div className="mb-3 form-check">
-                                    <input name={"acept"} type="checkbox" className="form-check-input" id="exampleCheck1" onChange={(e) => inputChangeHandler(e)} />
-                                    <label className="form-check-label" htmlFor="exampleCheck1">Acepta los términos y condiciones</label>
-                                    {
-                                        errors.acept && <div id="metrosHelp" className="form-text text-danger">{errors.acept}</div>
-                                    }
-                                </div>
-                                <button type="submit" className="btn btn-primary">Consultar</button>
-                            </form>
+            <div className="card-group mt-5 mb-5">
+                {
+                    features.length > 1 && features.map((feature, index) => (
+                        <div className="card mx-auto" key={index}>
+                            <div className="card-header">
+                                {feature.attributes.OBJECTID}
+                            </div>
+                            <div className="card-body">
+                                <p>Comuna: {feature.attributes.COMUNA}</p>
+                                <p>Valor M2: $ {addDotThousands(feature.attributes.VALOR_M2)}</p>
+                                <p>Vigencia: {feature.attributes.VIGENCIA}</p>
+                                <p>Valor Consultado: $ {addDotThousands(feature.attributes.VALOR_M2 * parseInt(formData.metros))}</p>
+                                <p>Valor a pagar: $ {addDotThousands(parseInt((feature.attributes.VALOR_M2 * parseInt(formData.metros))*1.15))}</p>
+                            </div>
                         </div>
-                    </>
-            }
-        </div>
+                    ))
+                }
+            </div>
+        </>
     )
 }
 
--- src/Service/ApiService.js
+++ src/Service/ApiService.js
@@ -1,12 +1,41 @@
 const ApiService = {}
 
-const OBTAIN_CBL_API_URL = `https://www.medellin.gov.co/site_consulta_pot/BuscarFichaMat.hyg`
+const SERVER = `http://localhost:4000`
 
-ApiService.obtainCBL = async (matricula) => {
-    const obtaintCBL = await fetch(`${OBTAIN_CBL_API_URL}?buscar=${matricula}`,{
-        method:'GET'
+ApiService.obtainCBL = async (OBTAIN_CBL_API_URL, matricula) => {
+    let formData ={
+        url:OBTAIN_CBL_API_URL,
+        params:matricula
+    }
+
+    const obtaintCBL = await fetch(`${SERVER}/api/cbml/get`,{
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
     })
-    console.log(obtaintCBL)
+    // console.log(obtaintCBL)
+    if(!obtaintCBL.ok){
+        let response = {
+            code: '500',
+            msg:"Hubo un error al traer la información"
+        }
+        return response
+    }
+    let data = await obtaintCBL.json()
+    let response = {
+        code:data.code,
+        msg: data.msg,
+        data: data.data
+    }
+    return response
+}
+
+ApiService.obtainPOT48Data = async (cbml) => {
+    const OBTAIN_POT48_DATA_API_URL = `https://www.medellin.gov.co/mapas/rest/services/ServiciosPlaneacion/POT48_Base/MapServer/1/query?where=cbml=${cbml}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=102100&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=OBJECTID,CBML&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=pjson`
+    const obtaintCBL = await fetch(`${OBTAIN_POT48_DATA_API_URL}`)
+    // console.log(obtaintCBL)
     if(!obtaintCBL.ok){
         let response = {
             code: '500',
@@ -17,7 +46,28 @@ ApiService.obtainCBL = async (matricula) => {
     let data = await obtaintCBL.json()
     let response = {
         code:'200',
-        msg: "sucess",
+        msg: "success",
+        data: data
+    }
+    return response
+}
+
+ApiService.obtainValueM2 = async (xmin,ymin,xmax,ymax,spartialReference) => {
+    const spartian = `{"wkid":${spartialReference.wkid},"latestWkid":${spartialReference.latestWkid}}`
+    const OBTAIN_M2_VALUE_DATA_API_URL = `https://www.medellin.gov.co/servidormapas/rest/services/vivienda_ciudad_terri/VC_Catastro_VCT/MapServer/1/query?f=JSON&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry={"xmin":${xmin},"ymin":${ymin},"xmax":${xmax},"ymax":${ymax},"spatialReference":${spartian}}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100`
+    const obtaintM2Value = await fetch(`${OBTAIN_M2_VALUE_DATA_API_URL}`)
+    // console.log(obtaintCBL)
+    if(!obtaintM2Value.ok){
+        let response = {
+            code: '500',
+            msg:"Hubo un error al traer la información"
+        }
+        return response
+    }
+    let data = await obtaintM2Value.json()
+    let response = {
+        code:'200',
+        msg: "success",
         data: data
     }
     return response
